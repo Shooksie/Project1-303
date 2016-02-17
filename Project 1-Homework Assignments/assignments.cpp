ea91332435bc65cdc8bfffe9d95d6b0301915896
Sami Shukri <ss9td@mail.umkc.edu>
2016-02-17 15:11:19 -0600

    assignment functions

---
+++ Project 1-Homework Assignments/assignments.cpp
@@ -0,0 +1,21 @@
+#include "Header.h"
+
+
+string assignments::getDueDate() {
+
+}
+
+string assignments::getName() {
+
+}
+void assignments::changecomplete() {
+
+}
+
+void assignments::setDueDate(int day, int month, int year, int hour, int min) {
+
+}
+
+void assignments::setName(string name) {
+
+}
