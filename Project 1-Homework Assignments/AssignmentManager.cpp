db3d49cf66f33344ab1de067f60920f108a77d20
Sami Shukri <ss9td@mail.umkc.edu>
2016-02-17 15:19:16 -0600

    Assignment Manager cpp

---
+++ Project 1-Homework Assignments/AssignmentManager.cpp
@@ -0,0 +1,28 @@
+#include "Header1.h"
+
+using namespace std;
+
+
+void assignmentManager::addAssignment(assignments newassign) {
+
+}
+
+void assignmentManager::checkComplete() {
+
+}
+
+void assignmentManager::checkUncomplete() {
+
+}
+
+void assignmentManager::printComp() {
+
+}
+
+void assignmentManager::printOrdered() {
+
+}
+
+void assignmentManager::printUncomp() {
+
+}
