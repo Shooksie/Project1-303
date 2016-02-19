9893e13018c9864a08256da9dce2187fcb2e007a
sabby918 <skst4b@mail.umkc.edu>
2016-02-19 16:22:48 -0600

    Update Main.cpp
    /* just an idea of how the menu should go. possibly go back later and create functions for repetitive parts*/

--- Project 1-Homework Assignments/Main.cpp
+++ Project 1-Homework Assignments/Main.cpp
@@ -9,4 +9,47 @@ void main() {
 	assignmentManager MainAssignments;
 	assignments assignmentNew;
 	MainAssignments.addAssignment(assignmentNew);
+	
+	
+	///////////////////////////////////////////////////////////////////////////Sarah
+	int answer;
+	cout << "Welcome to Assignment Manager!" << endl << endl << "Would you like to being? " << "Yes = 1  &  No = 0" << endl;
+	cin >> answer;
+	while (answer != 0 && answer != 1 ) {
+		cout << endl << "Not Valid Input.  Please Try Again." << endl;
+		cin >> answer;
+	}
+
+	if (answer == 1) {
+		cout << endl << setw(12) << "MENU" << endl << endl;
+		cout << "1 Display Assignments" << endl << "2 Add Assignment" << endl << "3 Edit Due Date" << endl << "4 Edit Description" << endl << "5 Complete Assignment"
+			 << endl << "6 Number of Late Assignments" << endl << "7 Save" << endl << "8 Exit" << endl << endl;
+		cout << "Enter a digit that corresponds with an action. " << endl;
+		cin >> answer;
+		while (answer != 1 && answer != 2 &&  answer != 3 && answer != 4 && answer != 5 && answer != 6 && answer != 7 && answer != 8) {
+			cout << endl << "Not Valid Input.  Please Try Again." << endl;
+			cin >> answer;
+		}
+		if (answer == 1){}
+		if (answer == 2){}
+		if (answer == 3){}
+		if (answer == 4){}
+		if (answer == 5){}
+		if (answer == 6){}
+		if (answer == 7){}
+		if (answer == 8){}
+
+
+
+
+
+	}
+	else if (answer == 0) {
+		cout << endl << "Good Bye!" << endl;
+	}
+
+
+	system("pause");
+	
+	//////////////////////////////////////////////////////////////////////////Sarah
 }
