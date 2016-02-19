299b78bc06dc592e45390a6bb8da181dfcb23bfe
baileyms <bmsv6c@mail.umkc.edu>
2016-02-19 00:03:11 -0600

    Update Assignment.h

--- Project 1-Homework Assignments/Assignment.h
+++ Project 1-Homework Assignments/Assignment.h
@@ -13,12 +13,13 @@ private:
 	string assignmentName, assignedDate, status;
 	bool completed=false;
 	
-	
 public:
 	bool getComplete();//returns the status of the assignment.
 	void getReadData(istream& in);
 	string getDueDate();
 	string getName();
+	string getAssignedDate();
+	string getStatus();
 	void setName(string name);
 	void changecomplete();
 	void printAssignment(ostream& output);
@@ -27,3 +28,7 @@ public:
 };
 
 #endif
+
+/* Shouldn't dueDate and assignedDate be Dates and not strings? 
+In that case we would not need date1 or date2. 
+Just wondering how these all tie together -Bailey */
