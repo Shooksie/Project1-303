dfe178d507b755d16692a614bbaa0053505aba94
sabby918 <skst4b@mail.umkc.edu>
2016-02-22 14:57:56 -0600

    Update Main.cpp
    Update... having trouble at line 130

--- Project 1-Homework Assignments/Main.cpp
+++ Project 1-Homework Assignments/Main.cpp
@@ -55,33 +55,83 @@ void main() {
 		if (answer == 3) {}
 		if (answer == 4) {}
 		if (answer == 5) {
-		
-		int the_Month;
-		int the_Day;
-		int the_Year;
+		int int_Month;
+		int int_Day;
+		int int_Year;
+		string the_Month;
+		string the_Day;
+		string the_Year;
 		string the_Date;
 
-		cout << "Enter the date of the assigned homework you wish to complete."<< endl;
+		cout << "Enter the date of the assigned homework you wish to complete." << endl;
 		cout << "Month (as a number) : ";
-		cin >> the_Month;
+		cin >> int_Month;
 		cout << "Day (as a number) : ";
-		cin >> the_Day;
+		cin >> int_Day;
 		cout << "Year (as a number) : ";
-		cin >> the_Year;
+		cin >> int_Year;
+
+		//Take into consideration leap year?? 
 
 
-		stringstream convert_Month, convert_Day, convert_Year; 
-
-		convert_Month << the_Month;
-		convert_Day << the_Day;
-		convert_Year << the_Year;
-
-		the_Date = convert_Month.str();
-		the_Date = the_Date.append("-") + convert_Day.str();
-		the_Date = the_Date.append("-") + convert_Year.str();
-
-		cout << the_Date;
+		//validating user input
+		while (int_Month > 12 || int_Month <= 0) {
+			cout << "You Entered an Invalid Month. Please Try again" << endl;
+			cout << "Month (as a number) : ";
+			cin >> int_Month;
+			cout << "Day (as a number) : ";
+			cin >> int_Day;
+			cout << "Year (as a number) : ";
+			cin >> int_Year;
 		}
+
+		//validating user input
+		while (int_Day > 31 || int_Day <= 0) {
+			cout << "You Entered an Invalid Day. Please Try again" << endl;
+			cout << "Month (as a number) : ";
+			cin >> int_Month;
+			cout << "Day (as a number) : ";
+			cin >> int_Day;
+			cout << "Year (as a number) : ";
+			cin >> int_Year;
+		}
+
+		//validating user input
+		while (int_Year < 0) {
+			cout << "You Entered an Invalid Year. Please Try again" << endl;
+			cout << "Month (as a number) : ";
+			cin >> int_Month;
+			cout << "Day (as a number) : ";
+			cin >> int_Day;
+			cout << "Year (as a number) : ";
+			cin >> int_Year;
+		}
+
+
+		stringstream convert_Month, convert_Day, convert_Year;
+
+		// typecasting
+		convert_Month << int_Month;
+		convert_Day << int_Day;
+		convert_Year << int_Year;
+
+		the_Month = convert_Month.str();
+		the_Day = convert_Day.str();
+		the_Year = convert_Year.str();
+
+		//creating the date as a string (user friendly)
+		the_Date = the_Month + "-" + the_Day + "-" + the_Year;
+
+
+		// look through uncompleted assignments to see if there is an assignment with that assigned date
+		//if there is, change the status to complete
+		if (MainAssignments.searchAssignment(the_Date) == true) {
+			cout << "Changing this assignment to complete!" << endl;
+			//THIS IS WHERE IM HAVING A LOGIC ISSUE
+		}
+		//if not valid date, let user know
+		else
+			cout << "Not a real assignment" << endl;
 		if (answer == 6) {}
 		if (answer == 7) {}
 		if (answer == 8) {}
