1b3fb868fd81a214c13da8cee0ed13d0045e0374
sabby918 <skst4b@mail.umkc.edu>
2016-02-23 10:54:47 -0600

    Create AM.h

---
+++ AM.h
@@ -0,0 +1,27 @@
+#include "Assignment.h"
+
+using namespace std;
+
+
+class assignmentManager {
+private:
+	list<assignment> completed;
+	list<assignment> uncomplete;
+	int number_Completed;
+	int number_Uncompleted;
+	int total_Assignments;
+	int number_Late;
+public:
+	bool searchAssignment(string assignedDate);
+	void checkComplete();
+	void checkUncomplete();
+	int getNumberCompleted();
+	int getNumberLate();
+	int getNumberUncompleted();
+	int getNumberofAssignments();
+	void addAssignment(assignment newassign);
+	void sortlist(list<assignment> assignList);
+	void printOrdered();
+	void printComp();
+	void printUncomp();
+};
